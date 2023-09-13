9b83076c9f3af8423b8ccb723fd6c28361b72ccf
jhakjung <nahak@naver.com>
2023-09-13 12:29:41 +0900

    2.62R 스크롤바 구현

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ function enqueue_custom_scripts() {
 	wp_enqueue_style('main-css', get_theme_file_uri('/assets/styles/bootstrap.css'));
 	wp_enqueue_script('fa-js', '//kit.fontawesome.com/61b7275f5f.js', 'NULL', '5.9.0', false);
 	wp_enqueue_script('main-js', get_theme_file_uri('bundled.js'), 'NULL', '1.0', true);
+	wp_enqueue_script('custom-js', get_theme_file_uri('/assets/scripts/custom.js'), array('jquery'), '1.0', true);
 	wp_enqueue_style('my-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
